b8ed5613121d6f7d05a9168bb3d783cbf3465ba8
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-04-17 15:04:09 +0700

    create mapping views

---
+++ resources/views/logis_vote/auth/login.blade.php
@@ -0,0 +1,24 @@
+
+    <!-- CARD -->
+    <div class="container_custom position-relative">
+      <div class="card text-center mb-3 px-4 py-4 position-absolute top-50 start-50 translate-middle" style="width: 30rem">
+        <div class="card-body">
+          <h2 class="card-title mb-5" style="font-weight: bold">SIGN IN</h2>
+          <!-- FORM LOGIN -->
+          <form action="">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
+            <div class="input-group mb-2">
+              <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" />
+            </div>
+            <div class="input-group mb-5 mr-5 text-end">
+              <a href="" style="color: black">Forgot Password?</a>
+            </div>
+            <input class="btn btn-primary px-4" type="submit" value="LOGIN" />
+          </form>
+          <!-- END FORM LOGIN -->
+        </div>
+      </div>
+    </div>
+    <!-- End CARD -->
