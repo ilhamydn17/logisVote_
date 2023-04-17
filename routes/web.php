1ad272397be0ba1a0c12e999b5119e693fe742d9
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-04-17 15:03:13 +0700

    create mapping views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('templates.app');
+});
+
+Route::get('/loginPage', function () {
+    return view('login');
 });
